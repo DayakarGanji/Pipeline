2bb6021c2dec830b7226cd7a139fc171090ffb05
DayakarGanji <58503863+DayakarGanji@users.noreply.github.com>
2020-08-05 16:46:00 +0530

    Update test_log-user.js

--- test/unit/test_log-user.js
+++ test/unit/test_log-user.js
@@ -1,5 +1,5 @@
 // var expect = require('expect.js');
-var sinon = require('sinon');
+// var sinon = require('sinon');
 
 // this is the javascript file that is under test
 var jsFile = '../../HelloWorld/apiproxy/resources/jsc/Logging-User-Setting-ID.js';
